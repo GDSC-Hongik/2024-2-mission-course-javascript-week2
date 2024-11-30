201b4c966fc2311a41bf02b1caffc72fe3c5d94c
tablemin03 <vs201466@naver.com>
2024-12-01 01:17:43 +0900

    [Add]: 미션3 제출

--- mission3/src/Button.js
+++ mission3/src/Button.js
@@ -1,10 +1,32 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Button = (props) => (
     <header>
-        <div className='container'>
-            <h1 className='logo'>logo</h1>
+        <div 
+        css={css`
+            text-align: center;
+            background: linear-gradient(180deg,black,yellow);
+            padding: 13% 10%;
+            
+
+            h1{
+            font-size: 100px;
+            color: black;
+            }
+
+            button{
+            border: solid;
+            border-radius: 10px;
+            background: yellow;
+            width: 100px;
+            height: 50px;
+            font-size: 20px;
+            }
+        `}>
+            <h1 className='logo'>MainPage</h1>
             <ul>
                 <button>
                     <Link to ="/picture">Picture</Link>
--- mission3/src/Picture.js
+++ mission3/src/Picture.js
@@ -1,8 +1,16 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 import tenet from './tenet.jpg';
 
 function Picture(){
     return (
-        <div className="pic">
+        <div 
+        css={css`
+            text-align: center;
+            background: linear-gradient(180deg,black,yellow);
+            padding: 5%;
+            height: 200%;
+        `}>
             <img src={tenet} alt="TENET" width="400" />
         </div>
     )
