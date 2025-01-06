038f2a3a50760352fe7b5a2035e60166ab841752
himinji <minji@g.hongik.ac.kr>
2025-01-06 11:42:30 +0900

    mod: Main to Home

--- mission3/src/App.js
+++ mission3/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Main from "./Home";
+import Home from "./Home";
 import Blog from "./Blog";
 import Picture from "./Picture";
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/picture" element={<Picture />} />
       </Routes>
